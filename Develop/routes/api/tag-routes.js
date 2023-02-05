ceade0fc6eb93d423155b888366831f4d47aebd9
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-02-05 12:31:22 -0800

    updating tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,7 +3,9 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -13,6 +15,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
   const tagData = await Tag.findByPk(req.params.id, {
+    include: [{ model: Product }],
   });
   if (!tagData) {
     res.status(404).json({ message: "No tags found." });
